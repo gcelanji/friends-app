68d180f2faf069b653c6e7eec8a446d272a8596e
Gerald <geraldcelanji@gmail.com>
2022-09-01 15:42:58 +0100

    Modify About page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
   end
 
   def about
-    @about_me = "This app was developed using Ruby 3.1.2 and Rails 7.0.3.1"
+    @about_me = "Friends App was developed as paer of the course Learn Ruby on Rails - Full Course from codemy.com. It was developed using Ruby 3.1.2 and Rails 7.0.3.1"
   end
 end
